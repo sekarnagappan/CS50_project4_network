beb207d015635ccd8aa3209ac9b3af66ee427bcd
sekarnagappan <sekar.nagappan@gmail.com>
2022-01-16 14:02:25 +0800

    Changed setting.py to use env variable for secrets

--- project4/settings.py
+++ project4/settings.py
@@ -30,17 +30,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = '13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8'
-if os.environ.get('GITHUB_WORKFLOW'):
-    SECRET_KEY = '13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8'
-else:
-    SECRET_KEY = env('SECRET_KEY')
+# to generate new keys.
+# from django.core.management.utils import get_random_secret_key
+# get_random_secret_key()
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if os.environ.get('GITHUB_WORKFLOW'):
-    DEBUG = True
-else:
-    DEBUG = env('DEBUG')
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
